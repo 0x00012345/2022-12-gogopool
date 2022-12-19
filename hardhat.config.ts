aeac771b92b34e76f9335d326f210a5ee3044484
zinc43 <a00083709@gmail.com>
2022-12-19 01:55:53 -0500

    feat: added functionallity to fork mainnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,13 +10,20 @@ import "@openzeppelin/hardhat-upgrades";
 import "hardhat-preprocessor";
 import "hardhat-storage-layout";
 
+import { accounts } from "./deploy/wallets";
+import {
+  buildForkConfig,
+  CHAINS_ID,
+  NETWORKS_RPC_URL,
+} from "./deploy/helper-hardhat-config";
+
 dotenv.config();
 
 // Load tasks
 const files = fs.readdirSync("./tasks");
 for (const file of files) {
-	if (!file.endsWith(".js")) continue;
-	require(`./tasks/${file}`);
+  if (!file.endsWith(".js")) continue;
+  require(`./tasks/${file}`);
 }
 
 // You need to export an object to set up your config
@@ -25,81 +32,87 @@ for (const file of files) {
 // "custom" network is what ANR calls itself, so we use that terminology
 
 function getRemappings() {
-	return fs
-		.readFileSync("remappings.txt", "utf8")
-		.split("\n")
-		.filter(Boolean)
-		.map((line) => line.trim().split("="));
+  return fs
+    .readFileSync("remappings.txt", "utf8")
+    .split("\n")
+    .filter(Boolean)
+    .map((line) => line.trim().split("="));
 }
 
 const config: HardhatUserConfig = {
-	solidity: {
-		version: "0.8.17",
-		settings: {
-			optimizer: {
-				enabled: true,
-				runs: 800,
-			},
-		},
-	},
-	defaultNetwork: "localhost",
-	networks: {
-		hardhat: {
-			accounts: {
-				mnemonic: process.env.MNEMONIC || "MISSING ENV MNEMONIC!",
-				accountsBalance: "1000000000000000000000000",
-			},
-		},
-		custom: {
-			url: `${process.env.ETH_RPC_URL}/ext/bc/C/rpc`,
-			gasPrice: 225000000000,
-			chainId: 43112,
-			accounts: {
-				mnemonic: process.env.MNEMONIC || "MISSING ENV MNEMONIC!",
-			},
-		},
-		fuji: {
-			url: "https://api.avax-test.network/ext/bc/C/rpc",
-			gasPrice: 225000000000,
-			chainId: 43113,
-			accounts: {
-				mnemonic: process.env.MNEMONIC || "MISSING ENV MNEMONIC!",
-			},
-		},
-		mainnet: {
-			url: "https://api.avax.network/ext/bc/C/rpc",
-			gasPrice: 225000000000,
-			chainId: 43114,
-			accounts: {
-				mnemonic: process.env.MNEMONIC || "MISSING ENV MNEMONIC!",
-			},
-		},
-	},
-	gasReporter: {
-		enabled: process.env.REPORT_GAS !== undefined,
-		currency: "USD",
-	},
-	etherscan: {
-		apiKey: process.env.ETHERSCAN_API_KEY,
-	},
-	preprocess: {
-		eachLine: (hre) => ({
-			transform: (line: string) => {
-				if (line.match(/^\s*import /i)) {
-					getRemappings().forEach(([find, replace]) => {
-						if (line.match(find)) {
-							line = line.replace(find, replace);
-						}
-					});
-				}
-				return line;
-			},
-		}),
-	},
-	paths: {
-		sources: "./contracts",
-		cache: "./cache",
-	},
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 800,
+      },
+    },
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    localhost: {
+      chainId: 1,
+      accounts: accounts.map(({ privateKey }) => privateKey),
+      allowUnlimitedContractSize: true,
+      forking: buildForkConfig(),
+    },
+    hardhat: {
+      chainId: 1,
+      accounts,
+      allowUnlimitedContractSize: true,
+      forking: buildForkConfig(),
+    },
+    custom: {
+      url: `${process.env.ETH_RPC_URL}/ext/bc/C/rpc`,
+      gasPrice: 225000000000,
+      chainId: 43112,
+      accounts: {
+        mnemonic: process.env.MNEMONIC || "MISSING ENV MNEMONIC!",
+      },
+    },
+    fuji: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: {
+        mnemonic: process.env.MNEMONIC || "MISSING ENV MNEMONIC!",
+      },
+    },
+    mainnet: {
+      url: "https://api.avax.network/ext/bc/C/rpc",
+      gasPrice: 225000000000,
+      chainId: 43114,
+      accounts: {
+        mnemonic: process.env.MNEMONIC || "MISSING ENV MNEMONIC!",
+      },
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  preprocess: {
+    eachLine: (hre) => ({
+      transform: (line: string) => {
+        if (line.match(/^\s*import /i)) {
+          getRemappings().forEach(([find, replace]) => {
+            if (line.match(find)) {
+              line = line.replace(find, replace);
+            }
+          });
+        }
+        return line;
+      },
+    }),
+  },
+  paths: {
+    sources: "./contracts",
+    cache: "./cache",
+  },
 };
 
 export default config;
